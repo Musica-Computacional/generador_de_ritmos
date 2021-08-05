a225f18b814ea26d9f58fbbd41203e0bedf0d1d9
sev17238 <sev17238@uvg.edu.gt>
2021-08-04 21:04:10 -0600

    initial project UI

---
+++ Assets/Scrypts/RythmGenerator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RythmGenerator : MonoBehaviour
+{
+
+    public string[] time_signatures = new string[] {"3/4","4/4"};
+    public double tempo = 60.0;
+
+
+    public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int[] clave_generator(string time_signature)
+    {
+
+    }
+
+    private int[] fill_generator(string time_signature)
+    {
+
+    }
+}
