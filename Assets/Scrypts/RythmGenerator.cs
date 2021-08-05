0ceece6e324f19432dc66e4f3ca15e45e1edaaa3
sev17238 <sev17238@uvg.edu.gt>
2021-08-05 00:04:33 -0600

    playing with random seeds and UI

--- Assets/Scrypts/RythmGenerator.cs
+++ Assets/Scrypts/RythmGenerator.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RythmGenerator : MonoBehaviour
 {
 
     public string[] time_signatures = new string[] {"3/4","4/4"};
     public double tempo = 60.0;
-
-
-    public
+    
+    public GameObject text_ran_gen_num;
+    public int num;
 
     // Start is called before the first frame update
     void Start()
     {
-           
-
+        
 
     }
 
@@ -25,13 +25,29 @@ public class RythmGenerator : MonoBehaviour
         
     }
 
+    /*void Awake()
+    {
+        seed = Random.Range(1, 99999);
+        Random.InitState(seed)
+    }*/
+
+    public void UpdateText()
+    {
+        num = Random.Range(1, 99999);
+        text_ran_gen_num.GetComponent<Text>().text = "" + num;
+        // return seed;
+    }
+
+
     private int[] clave_generator(string time_signature)
     {
-
+        int[] clave = new int[] { 1, 1, 1, 1, 1, 1 };
+        return clave;
     }
 
     private int[] fill_generator(string time_signature)
     {
-
+        int[] filler = new int[] { 1, 1, 1, 1, 1, 1 };
+        return filler;
     }
 }
---
+++ Assets/Scrypts/SeedGenerator.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedGenerator : MonoBehaviour
+{
+    public string string_seed = "seed";
+    public bool use_string_seed = true;
+    public bool random_seed = false;
+    public int seed = 1234;
+    public GameObject TextInput;
+    /*public void GenerateSeed()
+    {
+        if (use_string_seed)
+        {
+            seed = string_seed.GetHashCode();
+        }
+
+        if (random_seed)
+        {
+            seed = Random.Range(1, 99999);
+        }
+
+        Random.InitState(seed);
+        // return seed;
+    }*/
+
+
+    void Awake()
+    {
+        if (use_string_seed)
+        {
+            seed = string_seed.GetHashCode();
+        }
+
+        if (random_seed)
+        {
+            seed = Random.Range(1, 99999);
+        }
+
+        TextInput.GetComponent<Text>().text = "" + seed;
+
+        Random.InitState(seed);
+        // return seed;
+    }
+
+    void Start()
+    {
+        Debug.Log("yeah");
+        Debug.Log(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
