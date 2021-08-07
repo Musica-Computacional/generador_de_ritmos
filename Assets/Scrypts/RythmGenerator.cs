9ea517fcf811b682102ab7ca59bc1f37612afdf4
sev17238 <sev17238@uvg.edu.gt>
2021-08-07 17:40:17 -0600

    Calculos para generacion de Clave y Relleno listo

--- Assets/Scrypts/RythmGenerator.cs
+++ Assets/Scrypts/RythmGenerator.cs
@@ -7,9 +7,10 @@ using UnityEngine.UI;
 public class RythmGenerator : MonoBehaviour
 {
 
-    public string time_signature; //{ "3/4", "4/4" };
+    public string time_signature; // { "3/4", "4/4" };
     public string sub_division; // { "1/8", "1/16" };
     public double tempo = 60.0;
+    public bool remove_beats_randomly = false;
 
     public GameObject text_ran_gen_num;
     public int num;
@@ -18,8 +19,11 @@ public class RythmGenerator : MonoBehaviour
     void Start()
     {
         //DebugMethod();
-        List<string> actual_clave = ClaveGenerator(time_signature);
+        List<string> actual_clave = ClaveGenerator(time_signature,sub_division);
         Debug.Log("actual_clave: " + string.Join(",", actual_clave));
+        List<List<int>> fill_clave = FillerGenerator(actual_clave, sub_division);
+        Debug.Log("clave_pattern_o: " + string.Join(",", fill_clave[0]));
+        Debug.Log("fill_pattern: " + string.Join(",", fill_clave[1]));
     }
 
     // Update is called once per frame
@@ -32,13 +36,12 @@ public class RythmGenerator : MonoBehaviour
     {
         num = Random.Range(1, 99999);
         text_ran_gen_num.GetComponent<Text>().text = "" + num;
+
         // return seed;
     }
 
-    private List<string> ClaveGenerator(string time_signature)
+    private List<string> ClaveGenerator(string time_signature,string sub_division)
     {
-        int binNum;
-        string subdivision;
         List<string> all_possible_claves = new List<string>();
 
         if (time_signature == "3/4")
@@ -59,7 +62,6 @@ public class RythmGenerator : MonoBehaviour
         }
         else if (time_signature == "4/4")
         {
-            binNum = Random.Range(0, 1);
             if (sub_division == "1/8")
             {
                 // clave para 4/4 con 1/8 de subdivision
@@ -78,6 +80,8 @@ public class RythmGenerator : MonoBehaviour
                 all_possible_claves.Add("22222222");
             }
         }
+        Debug.Log("time_signature: " + time_signature + " sub_division: " + sub_division);
+        Debug.Log("all_possible_claves: " + string.Join(",", all_possible_claves));
 
         string chosen_clave = all_possible_claves[Random.Range(0, all_possible_claves.Count())];
         List<string> chosen_clave_list = new List<string>();
@@ -88,10 +92,121 @@ public class RythmGenerator : MonoBehaviour
         return chosen_clave_list;
     }
 
-    private int[] fill_generator(string time_signature)
+    private List<List<int>> FillerGenerator(List<string> clave, string sub_division)
     {
-        int[] filler = new int[] { 1, 1, 1, 1, 1, 1 };
-        return filler;
+        List<int> clave_pattern = new List<int>();
+        List<int> fill_pattern = new List<int>();
+        List<int> clave_pattern_o = new List<int>();
+
+        // Si la subdivision es en corcheas entonces podemos subdividir mas.
+        if (sub_division == "1/8")
+        {
+
+            for (int i = 0; i < clave.Count; i++)
+            {
+                if (clave[i] == "2")
+                {
+                    //clave_pattern.Add("4");
+                    clave_pattern.Add(1);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                }
+                else if (clave[i] == "3")
+                {
+                    //clave_pattern.Add("6");
+                    clave_pattern.Add(1);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                }
+            }
+            Debug.Log("clave_pattern +div: " + string.Join(",", clave_pattern));
+
+        }
+        else //(sub_division == "1/16")
+        {
+            for (int i = 0; i < clave.Count; i++)
+            {
+                if (clave[i] == "2")
+                {
+                    clave_pattern.Add(1);
+                    clave_pattern.Add(0);
+                }
+                else if (clave[i] == "3")
+                {
+                    clave_pattern.Add(1);
+                    clave_pattern.Add(0);
+                    clave_pattern.Add(0);
+                }
+            }
+            Debug.Log("clave_pattern: " + string.Join(",", clave_pattern));
+
+        }
+
+        //hacemos copia de la clave ya que aca ya esta construida
+        for (int i = 0;i < clave_pattern.Count; i++)
+        {
+            clave_pattern_o.Add(clave_pattern[i]);
+        }
+
+        // Convertimos los beats a silencios y bicerversa para obtener mas resultados
+        if (remove_beats_randomly)
+        {
+            // Contar numero de 1s en el arreglo
+            /*int beats_count = clave_pattern.Where(x => x.Equals("1")).Count();
+            Debug.Log("Beats Count: " + beats_count);
+            int ones_to_remove = Random.Range(1, beats_count - 1);
+            int start_removing = Random.Range(1, clave_pattern.Count);
+            for (int i = start_removing; i < clave_pattern.Count; i++)
+            {
+                if (clave_pattern[i] == "1")
+                {
+                    fill_pattern.Add(0);
+                }
+            }*/
+
+            
+
+            //fill_pattern = clave_pattern.OrderBy(i => System.Guid.NewGuid()).ToList();
+            clave_pattern = ShuffleList(clave_pattern);
+
+        }
+        
+        // hace shuffle de la lista
+        //clave_pattern = clave_pattern.OrderBy(i => System.Guid.NewGuid()).ToList();
+        for (int i = 0; i < clave_pattern.Count; i++)
+        {
+            if (clave_pattern[i] == 1)
+            {
+                fill_pattern.Add(0);
+            }
+            else
+            {
+                fill_pattern.Add(1);
+            }
+        }
+
+        List<List<int>> result = new List<List<int>>();
+        result.Add(clave_pattern_o);
+        result.Add(fill_pattern);
+        return result;
+    }
+
+    public List<int> ShuffleList(List<int> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
+        return ts;
     }
 
     private static List<string> FindPermutations(string set)
